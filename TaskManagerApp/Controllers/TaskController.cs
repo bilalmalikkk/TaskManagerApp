fbe81c833643c300cceca512ab33e6fa84d59712
Bilal Malik <bilalmalik746429@gmail.com>
2025-05-06 19:32:49 +0500

    Initial Commit

---
+++ TaskManagerApp/Controllers/TaskController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagerApp.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            // Sample Task object for demonstration
+            var task = new { Id = id, Title = "Sample Task", Description = "This is a task description" };
+            return View(task);
+        }
+
+    }
+}
+
+
